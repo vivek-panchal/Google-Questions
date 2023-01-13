9eb4b6058c3ba4c0996287208d6a09776c043a27
vivek <95127627+vivek-panchal@users.noreply.github.com>
2023-01-13 17:20:26 +0530

    Create Leetcode 1365. How Many Numbers Are Smaller Than the Current Number.cpp

---
+++ Leetcode 1365. How Many Numbers Are Smaller Than the Current Number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> ans;
+
+        for(int i=0;i<n;i++){
+            int cnt=0;
+            for(int j=0;j<n;j++){
+                if(nums[i]>nums[j]){
+                    cnt++;
+                }
+
+            }
+            ans.push_back(cnt);
+        }
+        return ans;
+    }
+};
