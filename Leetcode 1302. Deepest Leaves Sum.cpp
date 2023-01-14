f23d6ebff6fae1457d625368513a26723ce228de
vivek <95127627+vivek-panchal@users.noreply.github.com>
2023-01-14 18:06:06 +0530

    Create Leetcode 1302. Deepest Leaves Sum.cpp

---
+++ Leetcode 1302. Deepest Leaves Sum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int deepestLeavesSum(TreeNode* root) {
+        queue<TreeNode*> q;
+        q.push(root);
+        int sum=0;
+
+        while(!q.empty()){
+            int level=q.size();
+
+            sum=0;
+
+            while(level!=0){
+                TreeNode* temp=q.front();
+                q.pop();
+                sum+=temp->val;
+
+                if(temp->left){
+                    q.push(temp->left);
+                }
+
+                if(temp->right){
+                    q.push(temp->right);
+                }
+                level--;
+            }
+        }
+        return sum;
+    }
+};
