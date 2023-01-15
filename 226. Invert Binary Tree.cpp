724822c6ba33ed349d1626d603ca6db7ed2bd4c2
vivek <95127627+vivek-panchal@users.noreply.github.com>
2023-01-15 15:28:14 +0530

    Create 226. Invert Binary Tree.cpp

---
+++ 226. Invert Binary Tree.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+
+        if(root==NULL){
+            return NULL;
+        }
+
+        swap(root->left,root->right);
+
+        invertTree(root->left);
+        invertTree(root->right);
+
+        return root;   
+    }
+};
