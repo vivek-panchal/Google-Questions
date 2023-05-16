e4905366a2379c7b1c8c7413429a08968282374e
vivek <95127627+vivek-panchal@users.noreply.github.com>
2023-05-16 15:44:44 +0530

    Added solution - LeetHub

--- Bubble Sort - GFG/bubble-sort.c
+++ Bubble Sort - GFG/bubble-sort.c
@@ -22,13 +22,18 @@ void bubbleSort(int arr[], int n)
 {
     // Your code here 
     for(int i=n-1;i>0;i--){
+        int swap=0;
         for(int j=0;j<i;j++){
             if(arr[j]>arr[j+1]){
                 int temp=arr[j];
                 arr[j]=arr[j+1];
                 arr[j+1]=temp;
+                swap++;
             }
+
         }
+        if(swap==0)
+            break;
     }
 }
 
